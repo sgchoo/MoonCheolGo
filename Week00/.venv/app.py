d983a36352698265d22adae92fe8311b9d385af2
Eunjin <melong0523486@gmail.com>
2024-01-09 16:54:26 -0500

    no message

---
+++ Week00/.venv/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.dbmoonchul
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/main', methods=['POST'])
+def apply_moonchul():
+    subject_receive = request.form['subject_give']
+    argument_receive = request.form['argument_give']
+    position_1_receive = request.form['position_1_give']
+    position_2_receive = request.form['position_2_give']
+    db.moonchuls.insert_one({'subject': subject_receive, 'argument': argument_receive, 'position1': position_1_receive, 'position2': position_2_receive, 'isProceeding': 'True'})
+    return jsonify({'result': 'success'})
+
+
+@app.route("/show", methods=['GET'])
+def show_moonchuls():
+    result = list(db.moonchuls.find({},{'_id':0}))
+    return jsonify({'result': 'success', 'moonchuls': result})
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
